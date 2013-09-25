722f422374ce05cf6e5cebb82c76e89dda7785f3
Rodrigo Camara <rodrigoarenacamara@gmail.com>
2013-09-25 00:28:53 -0300

    First Commit

---
+++ Akangatu/frmLauncher.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLauncher
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblFechar = New System.Windows.Forms.Label()
+        Me.lblMinimizar = New System.Windows.Forms.Label()
+        Me.prgBar = New System.Windows.Forms.ProgressBar()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblNovidade2 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.lblNovidade1 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.lblNovidade3 = New System.Windows.Forms.Label()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.lblNovidade4 = New System.Windows.Forms.Label()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.lblNovidade5 = New System.Windows.Forms.Label()
+        Me.txtLogin = New System.Windows.Forms.TextBox()
+        Me.txtSenha = New System.Windows.Forms.TextBox()
+        Me.lblEntrar = New System.Windows.Forms.Label()
+        Me.lblEsqueceu = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.Panel6.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblFechar
+        '
+        Me.lblFechar.BackColor = System.Drawing.Color.Transparent
+        Me.lblFechar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblFechar.Location = New System.Drawing.Point(903, 5)
+        Me.lblFechar.Name = "lblFechar"
+        Me.lblFechar.Size = New System.Drawing.Size(41, 37)
+        Me.lblFechar.TabIndex = 1
+        '
+        'lblMinimizar
+        '
+        Me.lblMinimizar.BackColor = System.Drawing.Color.Transparent
+        Me.lblMinimizar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblMinimizar.Location = New System.Drawing.Point(847, 5)
+        Me.lblMinimizar.Name = "lblMinimizar"
+        Me.lblMinimizar.Size = New System.Drawing.Size(50, 37)
+        Me.lblMinimizar.TabIndex = 2
+        '
+        'prgBar
+        '
+        Me.prgBar.Location = New System.Drawing.Point(22, 476)
+        Me.prgBar.Name = "prgBar"
+        Me.prgBar.Size = New System.Drawing.Size(922, 30)
+        Me.prgBar.TabIndex = 4
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.lblNovidade2)
+        Me.Panel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel1.Location = New System.Drawing.Point(410, 271)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(534, 42)
+        Me.Panel1.TabIndex = 5
+        '
+        'lblNovidade2
+        '
+        Me.lblNovidade2.AutoSize = True
+        Me.lblNovidade2.Font = New System.Drawing.Font("Big Limbo AOE", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNovidade2.ForeColor = System.Drawing.Color.Honeydew
+        Me.lblNovidade2.Location = New System.Drawing.Point(3, 0)
+        Me.lblNovidade2.Name = "lblNovidade2"
+        Me.lblNovidade2.Size = New System.Drawing.Size(193, 48)
+        Me.lblNovidade2.TabIndex = 0
+        Me.lblNovidade2.Text = "Carregando..."
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Transparent
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.lblNovidade1)
+        Me.Panel2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel2.Location = New System.Drawing.Point(410, 223)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(534, 42)
+        Me.Panel2.TabIndex = 6
+        '
+        'lblNovidade1
+        '
+        Me.lblNovidade1.AutoSize = True
+        Me.lblNovidade1.Font = New System.Drawing.Font("Big Limbo AOE", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNovidade1.ForeColor = System.Drawing.Color.Honeydew
+        Me.lblNovidade1.Location = New System.Drawing.Point(3, 0)
+        Me.lblNovidade1.Name = "lblNovidade1"
+        Me.lblNovidade1.Size = New System.Drawing.Size(193, 48)
+        Me.lblNovidade1.TabIndex = 0
+        Me.lblNovidade1.Text = "Carregando..."
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.Transparent
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Controls.Add(Me.lblNovidade3)
+        Me.Panel3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel3.Location = New System.Drawing.Point(410, 319)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(534, 42)
+        Me.Panel3.TabIndex = 6
+        '
+        'lblNovidade3
+        '
+        Me.lblNovidade3.AutoSize = True
+        Me.lblNovidade3.Font = New System.Drawing.Font("Big Limbo AOE", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNovidade3.ForeColor = System.Drawing.Color.Honeydew
+        Me.lblNovidade3.Location = New System.Drawing.Point(3, 0)
+        Me.lblNovidade3.Name = "lblNovidade3"
+        Me.lblNovidade3.Size = New System.Drawing.Size(193, 48)
+        Me.lblNovidade3.TabIndex = 0
+        Me.lblNovidade3.Text = "Carregando..."
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.Transparent
+        Me.Panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel5.Controls.Add(Me.lblNovidade4)
+        Me.Panel5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel5.Location = New System.Drawing.Point(410, 371)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(534, 42)
+        Me.Panel5.TabIndex = 9
+        '
+        'lblNovidade4
+        '
+        Me.lblNovidade4.AutoSize = True
+        Me.lblNovidade4.Font = New System.Drawing.Font("Big Limbo AOE", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNovidade4.ForeColor = System.Drawing.Color.Honeydew
+        Me.lblNovidade4.Location = New System.Drawing.Point(3, 0)
+        Me.lblNovidade4.Name = "lblNovidade4"
+        Me.lblNovidade4.Size = New System.Drawing.Size(193, 48)
+        Me.lblNovidade4.TabIndex = 0
+        Me.lblNovidade4.Text = "Carregando..."
+        '
+        'Panel6
+        '
+        Me.Panel6.BackColor = System.Drawing.Color.Transparent
+        Me.Panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel6.Controls.Add(Me.lblNovidade5)
+        Me.Panel6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel6.Location = New System.Drawing.Point(410, 419)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(534, 42)
+        Me.Panel6.TabIndex = 7
+        '
+        'lblNovidade5
+        '
+        Me.lblNovidade5.AutoSize = True
+        Me.lblNovidade5.Font = New System.Drawing.Font("Big Limbo AOE", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNovidade5.ForeColor = System.Drawing.Color.Honeydew
+        Me.lblNovidade5.Location = New System.Drawing.Point(3, 0)
+        Me.lblNovidade5.Name = "lblNovidade5"
+        Me.lblNovidade5.Size = New System.Drawing.Size(193, 48)
+        Me.lblNovidade5.TabIndex = 0
+        Me.lblNovidade5.Text = "Carregando..."
+        '
+        'txtLogin
+        '
+        Me.txtLogin.Font = New System.Drawing.Font("Big Limbo AOE", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLogin.ForeColor = System.Drawing.Color.Peru
+        Me.txtLogin.Location = New System.Drawing.Point(116, 325)
+        Me.txtLogin.Name = "txtLogin"
+        Me.txtLogin.Size = New System.Drawing.Size(216, 45)
+        Me.txtLogin.TabIndex = 10
+        '
+        'txtSenha
+        '
+        Me.txtSenha.Font = New System.Drawing.Font("Big Limbo AOE", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSenha.ForeColor = System.Drawing.Color.Peru
+        Me.txtSenha.Location = New System.Drawing.Point(130, 377)
+        Me.txtSenha.Name = "txtSenha"
+        Me.txtSenha.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtSenha.Size = New System.Drawing.Size(202, 45)
+        Me.txtSenha.TabIndex = 11
+        '
+        'lblEntrar
+        '
+        Me.lblEntrar.BackColor = System.Drawing.Color.Transparent
+        Me.lblEntrar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblEntrar.Location = New System.Drawing.Point(221, 425)
+        Me.lblEntrar.Name = "lblEntrar"
+        Me.lblEntrar.Size = New System.Drawing.Size(111, 37)
+        Me.lblEntrar.TabIndex = 12
+        '
+        'lblEsqueceu
+        '
+        Me.lblEsqueceu.BackColor = System.Drawing.Color.Transparent
+        Me.lblEsqueceu.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblEsqueceu.Location = New System.Drawing.Point(19, 436)
+        Me.lblEsqueceu.Name = "lblEsqueceu"
+        Me.lblEsqueceu.Size = New System.Drawing.Size(129, 26)
+        Me.lblEsqueceu.TabIndex = 13
+        '
+        'frmLauncher
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Akangatu.My.Resources.Resources.fundo_launcher2
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(956, 518)
+        Me.Controls.Add(Me.lblEsqueceu)
+        Me.Controls.Add(Me.lblEntrar)
+        Me.Controls.Add(Me.txtSenha)
+        Me.Controls.Add(Me.txtLogin)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel6)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.prgBar)
+        Me.Controls.Add(Me.lblMinimizar)
+        Me.Controls.Add(Me.lblFechar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.Name = "frmLauncher"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Akangatu"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        Me.Panel6.ResumeLayout(False)
+        Me.Panel6.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblFechar As System.Windows.Forms.Label
+    Friend WithEvents lblMinimizar As System.Windows.Forms.Label
+    Friend WithEvents prgBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents lblNovidade2 As System.Windows.Forms.Label
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents lblNovidade3 As System.Windows.Forms.Label
+    Friend WithEvents Panel5 As System.Windows.Forms.Panel
+    Friend WithEvents lblNovidade4 As System.Windows.Forms.Label
+    Friend WithEvents Panel6 As System.Windows.Forms.Panel
+    Friend WithEvents lblNovidade5 As System.Windows.Forms.Label
+    Friend WithEvents txtLogin As System.Windows.Forms.TextBox
+    Friend WithEvents txtSenha As System.Windows.Forms.TextBox
+    Friend WithEvents lblEntrar As System.Windows.Forms.Label
+    Friend WithEvents lblNovidade1 As System.Windows.Forms.Label
+    Friend WithEvents lblEsqueceu As System.Windows.Forms.Label
+
+End Class
---
+++ Akangatu/frmLauncher.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmLauncher
+    Private Sub lblFechar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblFechar.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub frmLauncher_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lblMinimizar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblMinimizar.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+End Class
